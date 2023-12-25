22d3a98345a5000933ee5e2906bc774927f4d152
Mikita2703 <mikita.tsyhankou@gmail.com>
2023-12-26 00:31:32 +0300

    finish

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,113 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.roots = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.roots;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data);
+    this.roots === null
+      ? (this.roots = newNode)
+      : this.insertNode(this.roots, newNode);
+  }
+  insertNode(node, newNode) {
+    if (newNode.data >= node.data) {
+      node.right === null
+        ? (node.right = newNode)
+        : this.insertNode(node.right, newNode);
+    } else {
+      node.left === null
+        ? (node.left = newNode)
+        : this.insertNode(node.left, newNode);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (this.find(data) != null) return true;
+    else return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return search(this.roots, data);
+    function search(node, data) {
+      if (node === null) {
+        return null;
+      } else if (data < node.data) {
+        return search(node.left, data);
+      } else if (data > node.data) {
+        return search(node.right, data);
+      } else {
+        return node;
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.roots = removeNode(this.roots, data);
+    function removeNode(node, data) {
+      if (node === null) {
+        return null;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (data > node.data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (node.left === null && node.right === null) {
+          node = null;
+          return node;
+        }
+        if (node.left === null) {
+          node = node.right;
+          return node;
+        } else if (node.right === null) {
+          node = node.left;
+          return node;
+        }
+        let newNode = minNode(node.right);
+        node.data = newNode.data;
+        node.right = removeNode(node.right, newNode.data);
+        return node;
+        function minNode(node) {
+          if (node.left === null)
+            return node;
+          else
+            return minNode(node.left);
+        }
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return minNode(this.roots).data;
+    function minNode(node) {
+      if (node.left != null)
+        return minNode(node.left);
+      else
+        return node
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return maxNode(this.roots).data;
+    function maxNode(node) {
+      if (node.right != null)
+        return maxNode(node.right);
+      else
+        return node;
+    }
   }
 }
 
--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.start = null;
+    this.end = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.start;
+  }
+
+  enqueue(elem) {
+    let node = new ListNode(elem);
+
+    if (this.start) {
+      this.end.next = this.end = node;
+    } else {
+      this.start = this.end = node;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let start = this.start;
+    this.start = this.start.next;
+
+    return start.value;
   }
 }
 
